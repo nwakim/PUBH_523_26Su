b40c2db14f610167ba376b6a32bad764628621b6
Nicky Wakim <wakim@ohsu.edu>
2026-05-12 09:40:50 -0700

    adding index

--- Github_init.R
+++ Github_init.R
@@ -1,11 +1,12 @@
 ## Make sure hidden files from previous year are deleted (includes .git folder)
+### You can view hidden files by pressing "Command + Shift + ." on a Mac, or "Control + Shift + ." on Windows
 ## Go to Github, then repository page, then click "New"
 ## Make name for repository, then click "Create repository"
 ## Copy the code below and paste into terminal, making sure to edit the repository name if needed
 
 
 git init
-git remote add origin git@github.com:nwakim/BSTA_513_26S.git
+git remote add origin git@github.com:nwakim/PUBH_523_26Su.git
 git branch -M main
 git add --all
 git commit -m "first commit"
