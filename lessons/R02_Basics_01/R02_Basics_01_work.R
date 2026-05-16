c28316de8f0aff85cfa6649e719b53904cd9f794
Nicky Wakim <wakim@ohsu.edu>
2026-05-15 22:14:31 -0700

    adding more lessons

---
+++ lessons/R02_Basics_01/R02_Basics_01_work.R
@@ -0,0 +1,28 @@
+# Load packages
+library(dplyr)
+
+z = 7
+
+30+7
+
+(4^3) - (2 * 7) + (9/2)
+
+y = 3
+
+y = 5
+y = y + 1
+
+x = c(1, 2, 3, 4, 5)
+x = 1:5
+x = seq(from = 1, to = 5, by = 1)
+x = seq(8, 12, 1)
+
+hi = "hello"
+his = c("hello", "hi", "hey", "greetings", "salutations")
+his[2]
+x_subset = x[3:5]
+
+y = 1:5
+mean(x = 1:5)
+mean(y)
+
---
+++ lessons/R03_Basics_02/R03_Basics_02_work.R
@@ -0,0 +1,20 @@
+y = 8
+
+d = c(15:19, 22)
+d <- c(15,16,17,18,19,22)
+
+a = 3:6
+a + 2*a
+a + 2*(a)
+
+mean(d)
+d = c(d, NA)
+mean(d)
+mean(d, na.rm = T)
+d_mean = mean(d, na.rm=T)
+d
+mean(x = d, na.rm = T)
+
+(3+4)*5
+"hello"
+hello = 5
