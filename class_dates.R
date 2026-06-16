9a7e155ff5b0ce0880f6e6c68645a6b20c19216c
Nicky Wakim <wakim@ohsu.edu>
2026-06-16 16:13:46 -0700

    adding more lessons

--- class_dates.R
+++ class_dates.R
@@ -12,58 +12,58 @@ cal_dates = seq(from = first_day, to = last_day, by = "day")
 ## Week 1 dates
 w1d1 = cal_dates[1]
 w1d2 = cal_dates[3]
-hw0 = cal_dates[4]
+pract1 = cal_dates[7]
 
 ## Week 2 dates
 w2d1 = cal_dates[8]
 w2d2 = cal_dates[10]
-lab1 = cal_dates[14]
+pract2 = cal_dates[14]
 
 ## Week 3 dates
 w3d1 = cal_dates[15]
 w3d2 = cal_dates[17]
-hw1 = cal_dates[21]
+pract3 = cal_dates[21]
 
 ## Week 4 dates 
 w4d1 = cal_dates[22]
 w4d2 = cal_dates[24]
-hw2 = cal_dates[35]
-q1_open = cal_dates[24]
-q1_close = cal_dates[28]
+pract4 = cal_dates[28]
+lab1 = cal_dates[28]
 
 ## Week 5 dates
 w5d1 = cal_dates[29]
 w5d2 = cal_dates[31]
-lab2 = cal_dates[28]
+pract5 = cal_dates[35]
 
 ## Week 6 dates
 w6d1 = cal_dates[36]
 w6d2 = cal_dates[38]
-hw3 = cal_dates[49]
+pract6 = cal_dates[42]
+lab2 = cal_dates[42]
+
 
 ## Week 7 dates
 w7d1 = cal_dates[43]
 w7d2 = cal_dates[45]
-lab3 = cal_dates[42]
-q2_open = cal_dates[45]
-q2_close = cal_dates[49]
+pract7 = cal_dates[49]
+lab3 = cal_dates[49]
+
 
 ## Week 8 dates
 w8d1 = cal_dates[50]
 w8d2 = cal_dates[52]
-hw4 = cal_dates[63]
+pract8 = cal_dates[56]
 
 ## Week 9 dates
 w9d1 = cal_dates[57]
 w9d2 = cal_dates[59]
-lab4 = cal_dates[56]
+pract9 = cal_dates[63]
+lab4 = cal_dates[70]
 
 ## Week 10 dates
 w10d1 = cal_dates[64]
 w10d2 = cal_dates[66]
-hw5 = cal_dates[70]
-q3_open = cal_dates[59]
-q3_close = cal_dates[63]
+
 
 ## Week 11 dates
 w11d1 = cal_dates[71]
